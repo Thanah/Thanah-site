f244cebf72552535d799234fd2e6b4fb4d93699a
basedtr <70116305+basedtr@users.noreply.github.com>
2022-11-30 01:32:50 -0500

    Updates

---
+++ precache-manifest.df743844e65de54e5feef70d12fd1375.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "911d777a3463385ec0a9d6fd35d49676",
+    "url": "./index.html"
+  },
+  {
+    "revision": "af766d2d3247e1483458",
+    "url": "./static/css/2.61eb9e86.chunk.css"
+  },
+  {
+    "revision": "0c4b18387e9b3e68b708",
+    "url": "./static/css/main.9dc12c1b.chunk.css"
+  },
+  {
+    "revision": "af766d2d3247e1483458",
+    "url": "./static/js/2.972dfb9e.chunk.js"
+  },
+  {
+    "revision": "44ff4f981c23dd65ea65",
+    "url": "./static/js/3.04ed1a91.chunk.js"
+  },
+  {
+    "revision": "0c4b18387e9b3e68b708",
+    "url": "./static/js/main.9c858a28.chunk.js"
+  },
+  {
+    "revision": "a89fde7859c910a31bed",
+    "url": "./static/js/runtime~main.779d27a2.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "./static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "./static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "./static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "./static/media/slick.f97e3bbf.svg"
+  }
+]);
