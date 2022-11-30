b5ac1f044043d79f707e853f9e73f222a896967c
basedtr <70116305+basedtr@users.noreply.github.com>
2022-11-30 01:32:00 -0500

    removed homepage prop and replaced with env

---
+++ build/precache-manifest.d407b426262d5739574e9cd1553cf637.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5da932386de807b658abc365b926be36",
+    "url": "/thanah/index.html"
+  },
+  {
+    "revision": "413e2c54a5909256d60b",
+    "url": "/thanah/static/css/2.228496be.chunk.css"
+  },
+  {
+    "revision": "e520fe91348b8b5089ea",
+    "url": "/thanah/static/css/main.9dc12c1b.chunk.css"
+  },
+  {
+    "revision": "413e2c54a5909256d60b",
+    "url": "/thanah/static/js/2.972dfb9e.chunk.js"
+  },
+  {
+    "revision": "44ff4f981c23dd65ea65",
+    "url": "/thanah/static/js/3.04ed1a91.chunk.js"
+  },
+  {
+    "revision": "e520fe91348b8b5089ea",
+    "url": "/thanah/static/js/main.f5fdf6fd.chunk.js"
+  },
+  {
+    "revision": "0e38fdfd6cc396ca5cac",
+    "url": "/thanah/static/js/runtime~main.35c47273.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/thanah/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/thanah/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/thanah/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/thanah/static/media/slick.f97e3bbf.svg"
+  }
+]);
